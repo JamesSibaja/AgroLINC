289ffe3569447ceee9f1c1a693bd867b823d9fba
JamesSibaja <jsibajagranados2@gmail.com>
2026-06-04 14:22:53 -0600

    implementación de bolg (tercera)

--- js/blog.js
+++ js/blog.js
@@ -24,110 +24,103 @@ async function initBlog() {
 
 /* ========================= */
 
-function renderBlog() {
+function renderBlog(){
 
-const container =
-document.getElementById(
-"blogContainer"
-);
-
-container.innerHTML =
-"";
-
-const start =
-(page-1)
-*
-ITEMS;
-
-const subset =
-noticias.slice(
-start,
-start+ITEMS
-);
-
-subset.forEach(
-n=>{
-
-const div =
-document.createElement(
-"article"
-);
-
-div.className =
-"idea-modal-box";
-
-div.style.marginBottom =
-"3rem";
-
-div.innerHTML =
-`
-
-<p
-style="
-color:#6b7280;
-margin-bottom:1rem;
-"
->
-
-${n.fecha}
-
-</p>
-
-<h2>
-
-${n.titulo}
-
-</h2>
-
-<div
-class="blog-carousel"
->
-
-${
-n.imagenes
-.map(
-img=>`
-
-<img
-src="${img}"
-style="
-width:100%;
-border-radius:20px;
-margin-top:1rem;
-"
->
-
-`
-)
-.join("")
-}
-
-</div>
-
-<p
-style="
-margin-top:2rem;
-line-height:2;
-"
->
-
-${n.texto}
-
-</p>
-
-`;
-
-container.appendChild(
-div
-);
-
-}
-
-);
-
-renderPagination();
-
-}
+  const container =
+  document.getElementById(
+  "blogContainer"
+  );
+  
+  container.innerHTML="";
+  
+  const start =
+  (page-1)
+  *
+  ITEMS;
+  
+  const items =
+  noticias.slice(
+  start,
+  start+ITEMS
+  );
+  
+  items.forEach(
+  n=>{
+  
+  container.innerHTML +=
+  `
+  
+  <article
+  class="blog-post"
+  >
+  
+  <div
+  class="blog-gallery"
+  >
+  
+  ${
+  
+  n.imagenes
+  .length
+  
+  ?
+  
+  n.imagenes
+  .map(
+  img=>
+  
+  `<img src="${img}">`
+  
+  )
+  .join("")
+  
+  :
+  
+  `<img src="assets/images/hero.png">`
+  
+  }
+  
+  </div>
+  
+  <div
+  class="blog-body"
+  >
+  
+  <div
+  class="blog-date"
+  >
+  
+  ${n.fecha}
+  
+  </div>
+  
+  <h2
+  class="blog-title"
+  >
+  
+  ${n.titulo}
+  
+  </h2>
+  
+  <div
+  class="blog-text"
+  >
+  
+  ${n.texto}
+  
+  </div>
+  
+  </div>
+  
+  </article>
+  
+  `;
+  
+  });
+  
+  renderPagination();
+  
+  }
 
 /* ========================= */
 
