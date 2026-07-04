564cd75118665df83c04b69255c4c395cc71ea08
JamesSibaja <jsibajagranados2@gmail.com>
2026-07-04 15:08:53 -0600

    se mejora estilos para imprimir cartel de progreso arreglo manual

--- js/data.js
+++ js/data.js
@@ -636,7 +636,7 @@ function generarImagenRedesSociales() {
       medallasHTML += `
         <div class="compact-medal-slot medal-unlocked" title="${curso.nombre}">
           <div class="compact-medal-circle">
-            <i class="fa-solid${getCourseIcon(curso.nombre)}" style="padding-top : 0px"></i>
+            <i class="fa-solid ${getCourseIcon(curso.nombre)}" style="padding-top : 0px"></i>
           </div>
         </div>
       `;
